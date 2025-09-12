f88b7847d9a6e011e38b4a46fc74269c0180f2fd
Kanishq SIngh <singhkanishq06@gmail.com>
2025-09-12 13:57:58 +0530

    new commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { AppProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  <HashRouter>
     <AppProvider>
       <App />
     </AppProvider>
 
-  </BrowserRouter>,
+  </HashRouter>,
 )
